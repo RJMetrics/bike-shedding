42f156a815a8992acf23c181acc278c2777225a1
snelakanti <vnelakanti@gmail.com>
2018-02-11 08:49:40 -0500

    create material table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {MatButtonModule,MatCheckboxModule, MatTableDataSource, MatFormFieldModule, MatInputModule  } from '@angular/material';
+import {MatTableModule} from '@angular/material/table';
+import { HttpModule } from '@angular/http';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
-
+import { DataImportService } from './app.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatTableModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    HttpModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [DataImportService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
