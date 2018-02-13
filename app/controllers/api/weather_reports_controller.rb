f5bfe63428e1923f1b40abd4a33bcc5b9986c3ea
Jessica Wray <jessica@thrivetrm.com>
2018-02-12 21:26:19 -0500

    Namespace controllers under api/

---
+++ app/controllers/api/weather_reports_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  class WeatherReportsController < ApplicationController
+    def index
+      @weather_reports = WeatherReport.per_day
+      # This is a little nonstandard, but we're only interested in Q4 data here.
+      dates = BikeRide.pluck(:date).uniq
+      render json: @weather_reports.where(date: dates).to_json
+    end
+  end
+end
--- app/controllers/weather_reports_controller.rb
+++
@@ -1,8 +0,0 @@
-class WeatherReportsController < ApplicationController
-  def index
-    @weather_reports = WeatherReport.per_day
-    # This is a little nonstandard, but we're only interested in Q4 data here.
-    dates = BikeRide.pluck(:date).uniq
-    render json: @weather_reports.where(date: dates).to_json
-  end
-end
