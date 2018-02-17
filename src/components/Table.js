36a3d98c05efcad37f8b0a98c7d714cb1f53b5f0
Antonio Pugliese <apugliese74@gmail.com>
2018-02-17 15:52:54 -0500

    refactored

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,55 +1,46 @@
 import React from 'react';
 import ReactTable from 'react-table';
 
-class Table extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
+const Table = props => {
+  let data = props.json.map((ride) => {
+    ride.duration = +ride.duration
+    return ride
+  })
 
-    };
-  }
+  let columns = [{
+    Header: 'Trip ID',
+    accessor: 'trip_id'
+  },{
+    Header: 'Duration (Min)',
+    accessor: 'duration'
+  },{
+    Header: 'Passholder Type',
+    accessor: 'passholder_type'
+  }]
 
-  render() {
-    let data = this.props.json.map((ride) => {
-      ride.duration = +ride.duration
-      return ride
-    })
-
-    let columns = [{
-      Header: 'Trip ID',
-      accessor: 'trip_id'
-    },{
-      Header: 'Duration (Min)',
-      accessor: 'duration'
-    },{
-      Header: 'Passholder Type',
-      accessor: 'passholder_type'
-    }]
-
-    return(
-      <div>
-        <ReactTable
-          data={data}
-          columns={columns}
-          defaultPageSize={5}
-          getTrProps={(state, rowInfo, column) => {
-            return {
-              style: {
-                background: (rowInfo.row.duration > 30 && rowInfo.row.passholder_type === "Walk-up")
-                || (rowInfo.row.duration > 60 && rowInfo.row.passholder_type === "Indego30")
-                || (rowInfo.row.duration > 60 && rowInfo.row.passholder_type === "IndegoFlex")
-                || (rowInfo.row.duration > 30 && rowInfo.row.passholder_type === "One Day Pass")
-                 ? '#98FB98' : '#ffffff'
-              }
+  return(
+    <div>
+      <ReactTable
+        data={data}
+        columns={columns}
+        defaultPageSize={5}
+        getTrProps={(state, rowInfo, column) => {
+          return {
+            style: {
+              background: (rowInfo.row.duration > 30 && rowInfo.row.passholder_type === "Walk-up")
+              || (rowInfo.row.duration > 60 && rowInfo.row.passholder_type === "Indego30")
+              || (rowInfo.row.duration > 60 && rowInfo.row.passholder_type === "IndegoFlex")
+              || (rowInfo.row.duration > 30 && rowInfo.row.passholder_type === "One Day Pass")
+               ? '#98FB98' : '#ffffff'
             }
-          }}
-        />
-        <div className="table-information">
-          Rides where typeholders are charged extra for going above their initial allotted time are in green.
-        </div>
+          }
+        }}
+      />
+      <div className="table-information">
+        Rides where typeholders are charged extra for going above their initial allotted time are in green.
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Table;
--- src/containers/SVGContainer.js
+++ src/containers/SVGContainer.js
@@ -2,22 +2,13 @@ import React from 'react';
 import PieChart from '../components/PieChart';
 import AverageDurationChart from '../components/AverageDurationChart';
 
-class SVGContainer extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    };
-  }
-
-  render() {
-
-    return(
-      <div>
-        <AverageDurationChart json={this.props.json} />
-        <PieChart json={this.props.json}/>
-      </div>
-    );
-  }
+const SVGContainer = props => {
+  return(
+    <div>
+      <AverageDurationChart json={props.json} />
+      <PieChart json={props.json}/>
+    </div>
+  );
 };
 
 export default SVGContainer;
