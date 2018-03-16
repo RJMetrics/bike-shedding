fb8750789ff0c6157b4e35f406ccbbc3820ed08b
Eden Syoum <edensyoum@gmail.com>
2018-03-16 13:06:33 -0400

    Begin prototyping, add some color and style

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './styles/css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <main className="App">
+        <div className="content-area-one"/>
+        <div className="content-area-two"/>
+        <div className="content-area-three"/>
+      </main>
     );
   }
 }
