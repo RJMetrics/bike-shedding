a3cc782ff4169730aece988eb7f7c4914c6a6d85
Eden Syoum <edensyoum@gmail.com>
2018-03-16 13:40:30 -0400

    Add lodash, move indego.json into src, import into App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { Component } from 'react';
-import './styles/css/App.css';
+import React, { Component } from 'react'
+import { LineChart, Line } from 'recharts'
+import IndegoData from './data/indego.json'
+import _ from 'lodash'
+import './styles/css/App.css'
 
 class App extends Component {
   render() {
@@ -8,6 +11,10 @@ class App extends Component {
         <div className="content-area content-area-one"/>
         <div className="content-area content-area-two"/>
         <div className="content-area content-area-three"/>
+
+        <LineChart width={400} height={400}>
+          <Line type="monotone" stroke="#8884d8" />
+        </LineChart>
       </main>
     );
   }
