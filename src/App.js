c5074ce8f4a9be3e739f29f2651dafad4666fb1a
Eden Syoum <edensyoum@gmail.com>
2018-03-17 06:23:29 -0400

    Pull in data by mapping over byPassholder arrays, then use with BarChart

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { LineChart, Line } from 'recharts'
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 import IndegoData from './data/indego.json'
 import _ from 'lodash'
 import './styles/css/App.css'
@@ -7,15 +7,27 @@ import './styles/css/App.css'
 class App extends Component {
   render() {
     const byPassholder = _.groupBy(IndegoData, "passholder_type");
+    const data = _.map(byPassholder, function(trips, type) {
+      return {
+        passholder_type: type,
+        number_of_trips: trips.length
+      }
+    });
     return (
       <main className="App">
         <div className="content-area content-area-one"/>
         <div className="content-area content-area-two"/>
         <div className="content-area content-area-three"/>
 
-        <LineChart width={400} height={400}>
-          <Line type="monotone" stroke="#8884d8" />
-        </LineChart>
+        <BarChart width={600} height={300} data={data}
+          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+           <XAxis dataKey="passholder_type"/>
+           <YAxis dataKey="number_of_trips"/>
+           <CartesianGrid strokeDasharray="3 3"/>
+           <Tooltip/>
+           <Legend />
+           <Bar dataKey="number_of_trips" fill="#82ca9d" />
+        </BarChart>
       </main>
     );
   }
