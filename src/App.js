a9ae7824408b1cdf26317c107994718cf1f7a1d8
Eden Syoum <edensyoum@gmail.com>
2018-03-18 09:47:50 -0400

    Update names for clarity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { XAxis, YAxis, CartesianGrid, Tooltip, LineChart, Line } from 'recharts'
+import { XAxis, YAxis, CartesianGrid, Tooltip, AreaChart, Area, ResponsiveContainer } from 'recharts'
 import IndegoData from './data/indego.json'
 import _ from 'lodash'
 import moment from 'moment'
@@ -7,31 +7,35 @@ import './styles/css/App.css'
 
 class App extends Component {
   render() {
-    // const byPassholder = _.groupBy(IndegoData, "passholder_type");
-
-    // Lets group by start time instead of passholder type
-    let byStartTime = _.map(IndegoData, function(trip) {
-      trip.start_time = trip.start_time.substr(0, 13)
+    const withStartTime = _.map(IndegoData, function(trip) {
+      trip.starting_hour = trip.start_time.substr(0, 13)
       return trip
     });
-    byStartTime = _.groupBy(byStartTime, 'start_time');
-
-    const peakTimeData = _.map(byStartTime, function(trips, start_time) {
+    const byStartTime = _.groupBy(withStartTime, 'starting_hour');
+    const peakTimeData = _.map(byStartTime, function(trips, starting_hour) {
       return {
-        start_time: moment(start_time).format("MMM Do, h a"),
+        "Date": moment(starting_hour).format("MMM Do @ ha"),
         "Number of Trips": trips.length
       }
     });
+
     return (
       <main className="App">
-        <LineChart className={"content-area content-area-one"} width={600} height={300} data={peakTimeData}
-          margin={{top: 5, right: 30, left: 20, bottom: 5}}>
-        <XAxis dataKey="start_time" />
-        <YAxis />
-        <CartesianGrid strokeDasharray="3 3"/>
-        <Tooltip/>
-        <Line type="monotone" dataKey="Number of Trips" stroke="#8884d8" activeDot={{r: 8}}/>
-        </LineChart>
+      	<ResponsiveContainer width="90%" height="60%" className="content-area--wrapper">
+          <AreaChart data={peakTimeData} margin={{top: 5, right: 30, left: 20, bottom: 5}}>
+            <defs>
+              <linearGradient id="colorTrips" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8}/>
+                <stop offset="95%" stopColor="#8884d8" stopOpacity={0}/>
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="Date" />
+            <YAxis />
+            <CartesianGrid strokeDasharray="3 3"/>
+            <Tooltip/>
+            <Area type="monotone" dataKey="Number of Trips" stroke="#8884d8" fillOpacity={1} fill="url(#colorTrips)" activeDot={{r: 8}}/>
+          </AreaChart>
+      	</ResponsiveContainer>
       </main>
     );
   }
